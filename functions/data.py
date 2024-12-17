683be66325144d07487f2605d304c3cd17e9dc77
Adam Zabicki <azabicki@posteo.de>
2024-12-17 13:22:53 +0100

    add data.creafe_df() & data.load()

--- functions/data.py
+++ functions/data.py
@@ -3,3 +3,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+def create_df() -> pd.DataFrame:
+    """
+        creates a dataframe with the data for the user
+    """
+    cols = {"date":[], "weight":[], "fat":[], "water":[], "muscle":[]}
+    df = pd.DataFrame(cols)
+    return df
+
+
+def load() -> pd.DataFrame:
+    """
+        loads the data from the csv file
+    """
+    usr_name = st.session_state.user_name
+    db = pd.read_csv(os.path.join("data", usr_name + ".csv"))
+    db["date"] = pd.to_datetime(db["date"])
+    return db
+
