8d93396d01cf27051afb85df55803348b855c6a2
Adam Zabicki <azabicki@posteo.de>
2024-12-18 21:57:48 +0100

    new: trend figure

--- GravityLog.py
+++ GravityLog.py
@@ -27,3 +27,47 @@ with st.container(border=True):
         "style:", ["lines", "markers", "both"], index=0, key="fig_main_style",
     )
 
+
+# ----- trend figure -----
+ut.h_spacer(2)
+st.subheader("trend & predict")
+
+# draw trend figure and get trend
+fig_trend, trend = fgs.trend()
+
+with st.container(border=True):
+    # show current change rate of weight
+    c1,c2,c3 = st.columns([1, 1, 1])
+    c1.markdown("**weight changes:**")
+    c2.markdown(f"_{round(trend*10**9*60*60*24*7, 2)} kg/week_")
+    c3.markdown(f"_{round(trend*10**9*60*60*24*30, 2)} kg/month_")
+
+    # plot trend
+    st.plotly_chart(fig_trend, use_container_width=True, config = {'displayModeBar': False}, key="fig_trend")
+
+    with st.popover("trend settings"):
+        # radio button to select how to define starting point
+        st.radio(
+            "trend based on",
+            ["start date", "date range"],
+            key="trend_how",
+        )
+
+        # select _start date_
+        if st.session_state.trend_how == "start date":
+            st.date_input(
+                "select 'start date'",
+                format="YYYY-MM-DD",
+                key="trend_start",
+            )
+
+        # select _date range_
+        elif st.session_state.trend_how == "date range":
+            weeks = np.array([4, 8, 12, 16, 20, 24], dtype=np.int64)
+            st.selectbox(
+                "select 'range'",
+                weeks,
+                format_func=lambda x: str(x) + " weeks = " + str(x // 4) + " months",
+                key="trend_range",
+            )
+
--- functions/figures.py
+++ functions/figures.py
@@ -141,3 +141,215 @@ def main() -> go.Figure:
 
     return fig
 
+
+def trend() -> tuple[go.Figure, float]:
+    print(" > how: ", st.session_state.trend_how)
+    # get dates for x_axis based on trend_how
+    if st.session_state.trend_how == "start date":
+        # get date
+        print("  > start: ", st.session_state.trend_start)
+        print("         : ", type(st.session_state.trend_start))
+
+        x_data = [
+            datetime.combine(st.session_state.trend_start, time(0, 0, 0)),
+            list(st.session_state.db["date"])[-1],
+        ]
+
+    elif st.session_state.trend_how == "date range":
+        # get weeks
+        print("  > range: ", st.session_state.trend_range)
+        print("         : ", type(st.session_state.trend_range))
+
+        weeks = int(st.session_state.trend_range)
+        x_data = [
+            list(st.session_state.db["date"])[-1] - pd.Timedelta(weeks=weeks),
+            list(st.session_state.db["date"])[-1],
+        ]
+    else:
+        x_data = [
+            list(st.session_state.db["date"])[0],
+            list(st.session_state.db["date"])[-1],
+        ]
+
+    print("    > first day: ", x_data[0])
+    print("               : ", type(x_data[0]))
+
+    # filter data
+    idx_db = st.session_state.db["date"][st.session_state.db["date"] >= x_data[0]].index
+    db_data = st.session_state.db.iloc[idx_db]
+
+    # lin. reg. of relevant data
+    X = db_data["date"].values.reshape(-1, 1)
+    y = db_data["weight"].values
+    LR = LinReg().fit(X, y)
+    trnd = LR.coef_[0].item()
+    ntrcpt = LR.intercept_
+
+    # fit of actual data
+    y_fit = LR.predict(db_data["date"].values.astype(int).reshape(-1, 1))
+    db_data = db_data.assign(fit=y_fit)
+
+    # find weeks to be predicted (=w2p) - depending on if trend is going towards target
+    if (trnd < 0 and st.session_state.user_kg > db_data["weight"].iloc[-1]) or (
+        trnd > 0 and st.session_state.user_kg < db_data["weight"].iloc[-1]
+    ):
+        # 2 weeks to predict for unwanted trend
+        w2p = 2
+
+        # set target_flags
+        target_reached = False
+        target_late = False
+
+    else:
+        # calculate weeks until target is reached
+        date_on_target = pd.to_datetime(
+            (st.session_state.user_kg - ntrcpt) / trnd, unit="ns"
+        )
+        t_diff = date_on_target - db_data["date"].iloc[-1]
+        w2p = math.ceil(t_diff / np.timedelta64(1, "W")) + 1
+
+        # set target_flags
+        target_reached = True
+        target_late = False
+        if w2p > 51:
+            w2p = 51
+            target_late = True
+
+    # predict weight
+    pred_date = pd.date_range(
+        db_data["date"].iloc[-1] + pd.Timedelta(days=1),
+        db_data["date"].iloc[-1] + pd.Timedelta(weeks=w2p),
+        freq="d",
+    )
+    pred_weight = LR.predict(pred_date.values.astype(int).reshape(-1, 1)).round(2)
+
+    # calculate x-range
+    x_range = [
+        db_data["date"].iloc[0] - pd.Timedelta(weeks=1),
+        pred_date[-1],
+    ]
+
+    # instantiate figure
+    fig = go.Figure()
+
+    # return if no measurements stored
+    if st.session_state.db.shape[0] == 0:
+        return fig, 0
+
+    # add target weight
+    fig.add_trace(
+        go.Scatter(
+            x=x_range,
+            y=[st.session_state.user_kg, st.session_state.user_kg],
+            hoverinfo="skip",
+            name="target",
+            mode="lines",
+            line_color="#50C878",
+            line_width=2,
+        )
+    )
+
+    # add weight
+    fig.add_trace(
+        go.Scatter(
+            x=db_data["date"],
+            y=db_data["weight"],
+            name="weight",
+            mode="markers",
+            marker_size=10,
+            marker_color=clrs["weight"],
+        )
+    )
+
+    # add _FIT_
+    fig.add_trace(
+        go.Scatter(
+            x=db_data["date"],
+            y=db_data["fit"],
+            hoverinfo="skip",
+            name="weight",
+            mode="lines",
+            line_width=3,
+            line_color=clrs["trend"],
+        )
+    )
+
+    # add _PREDICTION_
+    fig.add_trace(
+        go.Scatter(
+            x=pred_date,
+            y=pred_weight[:, ],
+            name="prediction",
+            mode="lines",
+            line_width=3,
+            line_dash="dash",
+            line_color=clrs["prediction"],
+        )
+    )
+
+    # add _PREDICTION_TEXT_
+    if target_reached:
+        # find first day [index] of target reached
+        if target_late:
+            pred_target_date = -1
+            str_target = pred_date[pred_target_date].strftime("%d.%m.%y")
+            text = f"{str_target}<br>(> year)"
+
+        else:
+            pred_target_date = (
+                np.where(pred_weight < st.session_state.user_kg)[0][0]
+                if trnd < 0
+                else np.where(pred_weight > st.session_state.user_kg)[0][0]
+            )
+
+            str_target = pred_date[pred_target_date].strftime("%d.%m.%y")
+            str_days = pred_date[pred_target_date] - pd.Timestamp.today()
+            text = f"{str_target}<br>({str_days.days} days)"
+
+        # text_patch
+        fig.add_annotation(
+            x=pred_date[pred_target_date],
+            y=st.session_state.user_kg,
+            text=text,
+            showarrow=True,
+            arrowhead=3,
+            yanchor="bottom",
+            ay=-180 if trnd < 0 else 180,
+            ax=-40,
+            borderwidth=1,
+            borderpad=7,
+            bgcolor="#ffffff",
+        )
+
+    # set some layout properties
+    fig.update_layout(
+        # height of figure
+        height=350,
+
+        # turn legend off
+        showlegend=False,
+
+        # hovering
+        hovermode="x unified",
+        hoverlabel=dict(
+            font_size=12,
+            bgcolor="#fefefe"),
+        hoverdistance=1,
+
+        # margin
+        margin=dict(
+            l=0, r=0, t=0, b=0
+        ),
+    )
+
+    # set x/y-axes properties
+    fig.update_xaxes(
+        range=x_range,
+        type="date",
+        showgrid=True,
+    )
+    fig.update_yaxes(
+        ticksuffix=" kg",
+    )
+
+    return fig, trnd
